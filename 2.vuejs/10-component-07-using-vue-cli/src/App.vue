6314cb789348731c8d6acd76ecff4ab281a4b66b
Armin <datarivers.org@gmail.com>
2024-05-26 18:38:58 +0200

    add vue 10 component 07 using vue cli problem area

---
+++ 2.vuejs/10-component-07-using-vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>Friends List</h1>
+  </header>
+  <ul>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+          isFavorite: 1
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com"
+        },
+      ],
+    };
+  }
+}
+</script>
